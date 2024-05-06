bd492567ec1276fafa748e80417c849b875a011e
Andres Trujillo <andrest@zeusmanager.com>
2024-05-06 12:16:24 +0200

    Group by date

--- src/data/4_working_with_data.py
+++ src/data/4_working_with_data.py
@@ -1,30 +1,52 @@
+import pandas as pd
 import dask.dataframe as dd
 
+path_to_csv_folder = './data/raw/historical/*.csv'
+
+def extract_month_day(df):
+    df['last_reported'] = pd.to_datetime(df['last_reported'], unit='s')
+    df['month'] = df['last_reported'].dt.strftime('%m')
+    df['day'] = df['last_reported'].dt.strftime('%d')
+    df['hour'] = df['last_reported'].dt.strftime('%H')
+    return df
+
 dtype = {
-    'station_id': 'Int64',  # Assuming station_id has no non-integer values
-    'num_bikes_available': 'Int64',
-    'num_bikes_available_types.mechanical': 'Int64',
-    'num_bikes_available_types.ebike': 'Int64',
-    'num_docks_available': 'Int64',
-    'last_reported': 'string',  # If this column has no non-integer values, else consider 'object'
-    'is_charging_station': 'boolean',  # Will convert to boolean later
-    'status': 'category',
-    'is_installed': 'boolean',  # Will convert to boolean later
-    'is_renting': 'boolean',  # Will convert to boolean later
-    'is_returning': 'boolean',  # Will convert to boolean later
-    'last_updated': 'string',  # If this column has no non-integer values, else consider 'object'
+    'station_id': 'object', 
+    'num_bikes_available': 'object',
+    'num_bikes_available_types.mechanical': 'object',
+    'num_bikes_available_types.ebike': 'object',
+    'num_docks_available': 'object',
+    'last_reported': 'object',  
+    'is_charging_station': 'object',  
+    'status': 'object',
+    'is_installed': 'object',
+    'is_renting': 'object',  
+    'is_returning': 'object', 
+    'traffic': 'object',  
+    'last_updated': 'object',  
+    'ttl': 'object'  
 }
 
-folder = './data/processed/historical/*.csv'
+drop_columns    = [
+   'num_bikes_available', 
+   'num_bikes_available_types.mechanical',
+   'num_bikes_available_types.ebike',
+   'is_installed',
+   'is_renting',
+   'is_returning',
+   'is_charging_station',
+   'status',
+   'last_updated',
+   'ttl',
+]
 
-##parse_dates = ['last_updated', 'last_reported']
+ddf = dd.read_csv(path_to_csv_folder, na_values = ['NAN'], dtype = dtype).dropna(how='any').reset_index().drop(drop_columns, axis=1)
+ddf['num_docks_available'] = ddf['num_docks_available'].astype('Int64')
+ddf['last_reported'] = ddf['last_reported'].astype('Float64')
+ddf = ddf.map_partitions(extract_month_day)
+ddf = ddf.drop(['last_reported'], axis=1)
 
-## ddf = dd.read_csv(folder, dtype = dtype, parse_dates=parse_dates)
-ddf = dd.read_csv(folder, dtype = dtype)
+new_ddf = ddf.groupby(['station_id', 'month', 'day' ,'hour']).agg({'num_docks_available': 'mean'}).reset_index().compute()
+new_ddf.to_csv('./data/processed/groupby/stations.csv', index=False)
 
-# ddf['last_reported'] = dd.to_datetime(ddf['last_reported'], unit='s')
-# ddf['last_reported'] = dd.to_datetime(ddf['last_reported'], unit='s')
-
-print("num_bikes_available")
-print(ddf['num_bikes_available'].max().compute())
-print(ddf['num_bikes_available'].min().compute())
+print(new_ddf.head())
