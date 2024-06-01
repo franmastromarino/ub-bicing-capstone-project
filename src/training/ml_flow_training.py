0ef336eb5ac96908b1349b0f6b27585f858a1a9e
Ulises Rey <ulises.rey@univie.ac.at>
2024-06-01 17:13:24 +0200

    need to figure out why features is a numpy array

--- src/training/ml_flow_training.py
+++ src/training/ml_flow_training.py
@@ -27,6 +27,7 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.preprocessing import OneHotEncoder
 
 #This is not used, could be used for the subprocess
 my_username = "ulisesreytorne@gmail.com"
@@ -111,23 +112,38 @@ df.dropna(inplace=True)
 #df.rename(columns=lambda x: x.replace('ctx_', 'ctx-'), inplace=True)
 
 models = [
-    # LinearRegression(),
-    # LinearSVR(),
-    # DecisionTreeRegressor(),
-    # RandomForestRegressor(),
-    # GradientBoostingRegressor(),
+    LinearRegression(),
+    LinearSVR(),
+    DecisionTreeRegressor(),
+    RandomForestRegressor(),
+    GradientBoostingRegressor(),
     #"simple_lstm"
 
 ]
 
-chosen_features = [['ctx-4', 'ctx-3', 'ctx-2', 'ctx-1', 'altitude', 'laborable']]
+chosen_features = [["station_id", "month", "hour", "post_code", 'ctx-4', 'ctx-3', 'ctx-2', 'ctx-1', 'altitude', 'laboral_day']]
+categorical_features = ["station_id", "hour", "post_code"]
 
 
 for features in chosen_features:
   for model in models:
 
-    X = df[features] 
+    if categorical_features:
+      one_hot_encoder = OneHotEncoder(sparse_output=False)
+      one_hot_variables = one_hot_encoder.fit_transform(df[categorical_features])
+      encoded_df = pd.DataFrame(one_hot_variables, columns=one_hot_encoder.get_feature_names_out())
+      df = pd.concat([df, encoded_df], axis=1)
+      df.drop(columns=categorical_features, inplace=True)
+      features.append(one_hot_encoder.get_feature_names_out())
+      print(features)
+      print(type(features))
+      X = df
+    
+    else:
+      X = df[features] 
     y = df['percentage_docks_available']
+
+    
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
