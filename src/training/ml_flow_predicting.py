4943934e2daf8abc9bf54cbb7c1c8b062df140a1
Ulises Rey <ulises.rey@univie.ac.at>
2024-06-06 20:23:12 +0200

    working version of the predicting script

--- src/training/ml_flow_predicting.py
+++ src/training/ml_flow_predicting.py
@@ -1,8 +1,13 @@
 import mlflow
 # Predict on a Pandas DataFrame.
 import pandas as pd
+import os
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.discriminant_analysis import StandardScaler
 
-from ml_flow_utils import configure_databricks, setup_mlflow
+
+
+from mlflow_utils import configure_databricks, setup_mlflow
 
 #This is not used, could be used for the subprocess
 my_username = "ulisesreytorne@gmail.com"
@@ -21,9 +26,34 @@ logged_model = 'runs:/3ad33955f8dc4d01bdb52904cfedb79a/model'
 
 loaded_model = mlflow.pyfunc.load_model(logged_model)
 
+print(os.path.abspath(os.getcwd()))
+full_df = pd.read_csv("./data/processed/groupby/stations_final_2024.csv")
+
+features = ["weekday", "month", "hour", "post_code", "ctx-4", "ctx-3", "ctx-2", "ctx-1", "altitude", "laboral_day"]
+
+categorical_features = ["weekday", "month", "hour", "post_code"]
+
+y = full_df['percentage_docks_available']
+
+df = full_df[features]
+
+if categorical_features:
+    one_hot_encoder = OneHotEncoder(sparse_output=False)
+    one_hot_variables = one_hot_encoder.fit_transform(df[categorical_features])
+    encoded_df = pd.DataFrame(one_hot_variables, columns=one_hot_encoder.get_feature_names_out())
+    df = pd.concat([df, encoded_df], axis=1)
+    df.drop(columns=categorical_features, inplace=True)
+
+    X_test = df
+
+scaler = StandardScaler()
+X_test = scaler.fit_transform(X_test)
 
 
 
 
+y_pred = loaded_model.predict(X_test)
 
-loaded_model.predict(pd.DataFrame(data))
+prediction_df = pd.DataFrame(y_pred, columns=["percentage_docks_available"])
+prediction_df.index.name="index"
+prediction_df.to_csv("./data/processed/groupby/pred_stations_final_2024.csv")
